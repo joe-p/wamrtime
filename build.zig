5d6f30722167175d1595628a536ab78de4d4cdb4
Joe Polny <joepolny@gmail.com>
2025-10-21 15:02:01 -0400

    build wasm

--- build.zig
+++ build.zig
@@ -64,4 +64,27 @@ pub fn build(b: *std.Build) void {
 
     const check = b.step("check", "Check if foo compiles");
     check.dependOn(&exe_check.step);
+
+    const wasm_target = b.resolveTargetQuery(.{
+        .cpu_arch = .wasm32,
+        .os_tag = .freestanding,
+    });
+
+    const wasm_optimize = std.builtin.OptimizeMode.ReleaseSmall;
+
+    const wasm_mod = b.addModule("program", .{
+        .root_source_file = b.path("wasm-src/wasm.zig"),
+        .target = wasm_target,
+        .optimize = wasm_optimize,
+    });
+
+    const wasm_progrram = b.addExecutable(.{
+        .name = "program",
+        .root_module = wasm_mod,
+    });
+    wasm_progrram.import_memory = true;
+    wasm_progrram.entry = .disabled;
+    wasm_progrram.rdynamic = true;
+
+    b.installArtifact(wasm_progrram);
 }
---
+++ wasm-src/wasm.zig
@@ -0,0 +1,3 @@
+export fn program() u64 {
+    return 1;
+}
