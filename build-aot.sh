1478505253a94e181fb4d464832197dadecfbc83
Joe Polny <joepolny@gmail.com>
2025-10-21 15:27:57 -0400

    build.sh

--- build-aot.sh
+++
@@ -1,6 +0,0 @@
-CURR_DIR=$PWD
-WAMR_DIR=${PWD}/wasm-micro-runtime
-OUT_DIR=${PWD}/out
-
-${WAMR_DIR}/wamr-compiler/build/wamrc-2.4.3 --size-level=3 --format=aot --cpu=apple-m4 -o ${OUT_DIR}/wasm-apps/testapp.aot ${OUT_DIR}/wasm-apps/testapp.wasm 
-
