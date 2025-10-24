266ab113e050afd9283c69b8b28a92992327da46
Joe Polny <joepolny@gmail.com>
2025-10-24 12:47:33 -0400

    still not working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ffi::c_void;
+
 #[allow(warnings)]
 mod wamr {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
@@ -9,5 +11,24 @@ fn main() {
     unsafe {
         let t = wamr::get_package_type(aot_bytes.as_ptr(), aot_bytes.len() as u32);
         println!("Package type: {}", t);
+    }
+
+    unsafe {
+        const HEAP_SIZE: usize = 1024 * 1024 * 2;
+        let heap_buf: Vec<u8> = vec![0; HEAP_SIZE];
+
+        let mut init_args = wamr::RuntimeInitArgs {
+            mem_alloc_type: wamr::mem_alloc_type_t_Alloc_With_Pool,
+            running_mode: wamr::RunningMode_Mode_Interp,
+            mem_alloc_option: wamr::MemAllocOption {
+                pool: wamr::MemAllocOption__bindgen_ty_1 {
+                    heap_buf: heap_buf.as_ptr() as *mut c_void,
+                    heap_size: HEAP_SIZE as u32,
+                },
+            },
+            ..Default::default()
+        };
+
+        wamr::wasm_runtime_full_init(&mut init_args as *mut wamr::RuntimeInitArgs);
     }
 }
