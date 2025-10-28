1f22ac9bff25de1c596a0979d853f5c1e29b2a94
Joe Polny <joepolny@gmail.com>
2025-10-28 14:14:17 -0400

    build llvm first

--- build.sh
+++ build.sh
@@ -7,6 +7,15 @@ set -ex
 
 cd ${ROOT}
 
+cd ${WAMR_DIR}/wamr-compiler
+bash build_llvm.sh
+mkdir -p build
+cd build
+cmake .. -DWAMR_BUILD_PLATFORM=darwin
+make
+
+cd ${ROOT}
+
 echo "Building WAMR vmlib with CMake..."
 
 # Create build directory if it doesn't exist
@@ -16,13 +25,6 @@ cd build
 # Configure and build with CMake to get the vmlib
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make vmlib
-
-cd ${WAMR_DIR}/wamr-compiler
-bash build_llvm.sh
-mkdir -p build
-cd build
-cmake .. -DWAMR_BUILD_PLATFORM=darwin
-make
 
 cd ${ROOT}
 
