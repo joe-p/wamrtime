cae4e08030d72ba189b95758dc72a4e7e41a6c09
Joe Polny <joepolny@gmail.com>
2025-11-05 19:03:51 -0500

    slight refactor

--- wasm_crates/avm_complex/src/lib.rs
+++ wasm_crates/avm_complex/src/lib.rs
@@ -4,34 +4,33 @@ extern crate alloc;
 
 use algokit::{GlobalBytes, GlobalUint, avm_panic};
 
-const GLOBAL_UINT_VALUE: GlobalUint = GlobalUint::new(b"foo");
-const GLOBAL_BYTES_VALUE: GlobalBytes = GlobalBytes::new(b"foo");
+const GLOBAL_UINT: GlobalUint = GlobalUint::new(b"foo");
+const GLOBAL_BYTES: GlobalBytes = GlobalBytes::new(b"foo");
 
 #[unsafe(export_name = "program")]
 pub extern "C" fn program() -> u64 {
-    let mut value = GLOBAL_UINT_VALUE.get();
-    if value != 0 {
+    let val = GLOBAL_UINT.get();
+    if val != 0 {
         avm_panic();
     }
 
-    GLOBAL_UINT_VALUE.set(7);
-
-    value = GLOBAL_UINT_VALUE.get();
-
-    if value != 7 {
+    GLOBAL_UINT.set(7);
+    let new_val = GLOBAL_UINT.get();
+    if new_val != 7 {
         avm_panic();
     }
 
-    GLOBAL_UINT_VALUE.set(0);
-
-    GLOBAL_BYTES_VALUE.write(b"hello AVM!");
+    GLOBAL_BYTES.write(b"hello AVM!");
 
     let buf = &mut [0u8; 128];
-    let retrieved_value = GLOBAL_BYTES_VALUE.read(buf);
+    let retrieved_value = GLOBAL_BYTES.read(buf);
 
     if retrieved_value != b"hello AVM!".as_slice() {
         avm_panic();
     }
+
+    GLOBAL_UINT.set(0);
+    GLOBAL_BYTES.write(&[]);
 
     0
 }
