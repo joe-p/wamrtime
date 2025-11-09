f2e228b4372f9f50a1d819772441095c7f533a1a
Joe Polny <joepolny@gmail.com>
2025-11-09 09:00:05 -0500

    Revert "seperate init call threads"
    This reverts commit 755275170b26c8c36711d8618bf2eefce4716363.

--- crates/wamrtime/src/runtime_thread.rs
+++ crates/wamrtime/src/runtime_thread.rs
@@ -5,22 +5,21 @@ use crossbeam_channel::{Receiver, Sender, bounded};
 use crate::{
     program::Program,
     runtime::{HostGasCheckFn, WamrHostFunction, WamrRuntime},
-    wamr,
 };
 
-pub struct InitMessage {
-    program_bytes: Vec<u8>,
-    program_sender: Sender<Program>,
-}
-
-pub struct CallMessage {
-    program_receiver: Receiver<Program>,
-    result_sender: Sender<u64>,
+pub enum ProgramMessage {
+    NewProgram {
+        program_bytes: Vec<u8>,
+        program_sender: Sender<Program>,
+    },
+    CallProgram {
+        program_receiver: Receiver<Program>,
+        result_sender: Sender<u64>,
+    },
 }
 
 pub struct RuntimeThread {
-    init_message_sender: crossbeam_channel::Sender<InitMessage>,
-    call_message_sender: crossbeam_channel::Sender<CallMessage>,
+    program_message_sender: crossbeam_channel::Sender<ProgramMessage>,
 }
 
 impl RuntimeThread {
@@ -32,75 +31,70 @@ impl RuntimeThread {
         app_heap_size: usize,
         max_pages: u32,
     ) -> Self {
-        let (init_sender, init_receiver) = bounded::<InitMessage>(256);
-        let (call_sender, call_receiver) = bounded::<CallMessage>(256);
+        // We bound it with 512 because we can have 256 programs total, with each program having
+        // two messages (init and call). It is unlikely we'll ever get close to this limit, but it
+        // seems preferable to an unbounded channel.
+        let (prog_sender, prog_receiver) = bounded::<ProgramMessage>(512);
 
-        // Spawn initialization thread
         thread::spawn(move || {
             let _runtime = WamrRuntime::new(gas_check_fn, host_fns.clone(), runtime_heap_size)
                 .expect("Failed to create WamrRuntime");
 
-            while let Ok(init_message) = init_receiver.recv() {
-                let err_buf = &mut [0i8; crate::ERROR_BUFFER_SIZE];
-                let compiler = crate::compiler::Compiler::new();
+            while let Ok(program_message) = prog_receiver.recv() {
+                match program_message {
+                    ProgramMessage::NewProgram {
+                        program_bytes,
+                        program_sender,
+                    } => {
+                        let err_buf = &mut [0i8; crate::ERROR_BUFFER_SIZE];
+                        let program = Program::new(
+                            &mut program_bytes.clone(),
+                            err_buf,
+                            app_heap_size,
+                            stack_size,
+                            max_pages,
+                        )
+                        .expect("Failed to create Program");
 
-                let mut compiled_bytes = compiler
-                    .compile_wasm(&mut init_message.program_bytes.clone())
-                    .expect("Failed to compile WASM bytes");
+                        if err_buf[0] != 0 {
+                            let err_msg = unsafe { std::ffi::CStr::from_ptr(err_buf.as_ptr()) }
+                                .to_string_lossy()
+                                .into_owned();
+                            panic!("Error buffer not empty: {}", err_msg);
+                        }
 
-                let program = Program::new(
-                    &mut compiled_bytes,
-                    err_buf,
-                    app_heap_size,
-                    stack_size,
-                    max_pages,
-                )
-                .expect("Failed to create Program");
+                        program_sender
+                            .send(program)
+                            .expect("Failed to send initialized program");
+                    }
+                    ProgramMessage::CallProgram {
+                        program_receiver,
+                        result_sender,
+                    } => {
+                        let program = program_receiver
+                            .recv()
+                            .expect("Failed to receive program for calling");
 
-                if err_buf[0] != 0 {
-                    let err_msg = unsafe { std::ffi::CStr::from_ptr(err_buf.as_ptr()) }
-                        .to_string_lossy()
-                        .into_owned();
-                    panic!("Error buffer not empty: {}", err_msg);
+                        let result = program.call().expect("Failed to call program");
+
+                        result_sender
+                            .send(result)
+                            .expect("Failed to send program result");
+                    }
                 }
-
-                init_message
-                    .program_sender
-                    .send(program)
-                    .expect("Failed to send initialized program");
             }
-        });
-
-        // Spawn calling thread
-        thread::spawn(move || {
-            unsafe { wamr::wasm_runtime_init_thread_env() };
-            while let Ok(call_message) = call_receiver.recv() {
-                let program = call_message
-                    .program_receiver
-                    .recv()
-                    .expect("Failed to receive program for calling");
-
-                let result = program.call().expect("Failed to call program");
-
-                call_message
-                    .result_sender
-                    .send(result)
-                    .expect("Failed to send program result");
-            }
-            unsafe { wamr::wasm_runtime_destroy_thread_env() };
         });
 
         Self {
-            init_message_sender: init_sender,
-            call_message_sender: call_sender,
+            program_message_sender: prog_sender,
         }
     }
 
     pub fn init_program(&self, program_bytes: Vec<u8>) -> Receiver<Program> {
         let (sender, receiver) = crossbeam_channel::bounded::<Program>(1);
 
-        self.init_message_sender
-            .send(InitMessage {
+        self.program_message_sender
+            .send(ProgramMessage::NewProgram {
                 program_bytes,
                 program_sender: sender,
             })
@@ -112,8 +106,8 @@ impl RuntimeThread {
     pub fn call_intialized_program(&self, program_receiver: Receiver<Program>) -> u64 {
         let (result_sender, result_receiver) = bounded::<u64>(1);
 
-        self.call_message_sender
-            .send(CallMessage {
+        self.program_message_sender
+            .send(ProgramMessage::CallProgram {
                 program_receiver,
                 result_sender,
             })
